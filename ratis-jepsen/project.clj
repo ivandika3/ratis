09290e9dac8a6b370e4af4079b955ebd515317a8
Ivan Andika <ivandika@apache.org>
2025-07-01 22:56:51 +0800

    Phase 2: Implement real Apache Ratis integration with Java interop
    - Add Apache Ratis dependencies (client, common, grpc, server-api, proto, examples)
    - Replace MockRatisClient with RatisCounterClient using real Ratis Java classes
    - Implement comprehensive server lifecycle management in RatisDB
    - Add Java interop helper functions for RaftGroup and RaftClient building
    - Fix all syntax errors and ensure compilation success
    - Maintain backward compatibility with mock implementations
    - Add Phase 2 demo script with comprehensive validation tests
    - Update documentation with Phase 2 implementation details
    
    All unit tests passing, real Ratis components functional, ready for cluster deployment.

--- ratis-jepsen/project.clj
+++ ratis-jepsen/project.clj
@@ -5,7 +5,14 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [jepsen "0.3.9"]
-                 [cheshire "5.11.0"]]
+                 [cheshire "5.11.0"]
+                 ;; Apache Ratis dependencies for Phase 2
+                 [org.apache.ratis/ratis-client "3.1.3"]
+                 [org.apache.ratis/ratis-common "3.1.3"]
+                 [org.apache.ratis/ratis-grpc "3.1.3"]
+                 [org.apache.ratis/ratis-server-api "3.1.3"]
+                 [org.apache.ratis/ratis-proto "3.1.3"]
+                 [org.apache.ratis/ratis-examples "3.1.3"]]
   :exclusions [org.slf4j/slf4j-log4j12]
   :main jepsen.ratis.core
   :profiles {:uberjar {:aot :all}
--- ratis-jepsen/src/jepsen/ratis/client.clj
+++ ratis-jepsen/src/jepsen/ratis/client.clj
@@ -4,10 +4,135 @@
             [clojure.string :as str]
             [jepsen.client :as client]
             [jepsen.util :refer [timeout]]
-            [slingshot.slingshot :refer [try+]]))
+            [slingshot.slingshot :refer [try+]])
+  (:import [org.apache.ratis.client RaftClient RaftClientConfigKeys]
+           [org.apache.ratis.conf RaftProperties]
+           [org.apache.ratis.protocol RaftGroup RaftGroupId RaftPeer RaftPeerId]
+           [org.apache.ratis.examples.counter CounterCommand]
+           [org.apache.ratis.thirdparty.com.google.protobuf ByteString]
+           [java.net InetSocketAddress]
+           [java.util UUID]
+           [java.util.concurrent TimeUnit]))
 
 (def ^:dynamic *operation-timeout-ms* 5000)
 
+;; Helper functions for Ratis client integration
+
+(defn build-raft-group
+  "Build a RaftGroup from a list of node names"
+  [nodes]
+  (let [group-id (RaftGroupId/valueOf (UUID/fromString "02511d47-d67c-49a3-9011-abb3109a44c1"))
+        peers (map (fn [node]
+                    (-> (RaftPeer/newBuilder)
+                        (.setId (RaftPeerId/valueOf (str node)))
+                        (.setAddress (InetSocketAddress. (str node) 6000))
+                        (.build))) 
+                  nodes)]
+    (RaftGroup/valueOf group-id (into-array RaftPeer peers))))
+
+(defn build-raft-client
+  "Build a RaftClient for the given nodes"
+  [nodes]
+  (let [properties (RaftProperties.)
+        group (build-raft-group nodes)]
+    ;; Use default properties for now - specific configurations can be added later
+    (-> (RaftClient/newBuilder)
+        (.setProperties properties)
+        (.setRaftGroup group)
+        (.build))))
+
+(defn extract-counter-value
+  "Extract counter value from Ratis reply"
+  [reply]
+  (when (.isSuccess reply)
+    (let [message (.getMessage reply)]
+      (when message
+        (Long/parseLong (.toStringUtf8 message))))))
+
+(defrecord RatisCounterClient [client nodes]
+  client/Client
+  
+  (open! [this test node]
+    (info "Opening Ratis client connection to cluster with nodes:" (:nodes test))
+    (try+
+      (let [raft-client (build-raft-client (:nodes test))]
+        (info "Successfully created Ratis client")
+        (assoc this :client raft-client :nodes (:nodes test)))
+      (catch Exception e
+        (error "Failed to create Ratis client:" (.getMessage e))
+        (throw e))))
+  
+  (setup! [this test]
+    (info "Setting up Ratis client")
+    this)
+  
+  (invoke! [this test op]
+    (debug "Invoking operation:" op)
+    (if-not client
+      (assoc op :type :fail :error "No client connection")
+      (try+
+        (case (:f op)
+          :read
+          (let [reply (.sendReadOnly (.async client) 
+                                    (.getMessage CounterCommand/GET))]
+            (try
+              (let [response (.get reply *operation-timeout-ms* TimeUnit/MILLISECONDS)]
+                (if (.isSuccess response)
+                  (let [value (extract-counter-value response)]
+                    (assoc op :type :ok :value (or value 0)))
+                  (assoc op :type :fail :error (.toString response))))
+              (catch java.util.concurrent.TimeoutException _
+                (assoc op :type :info :error "timeout"))
+              (catch Exception e
+                (assoc op :type :fail :error (.getMessage e)))))
+          
+          :inc
+          (let [reply (.send (.async client)
+                            (.getMessage CounterCommand/INCREMENT))]
+            (try
+              (let [response (.get reply *operation-timeout-ms* TimeUnit/MILLISECONDS)]
+                (if (.isSuccess response)
+                  (assoc op :type :ok)
+                  (assoc op :type :fail :error (.toString response))))
+              (catch java.util.concurrent.TimeoutException _
+                (assoc op :type :info :error "timeout"))
+              (catch Exception e
+                (assoc op :type :fail :error (.getMessage e)))))
+          
+          :get
+          (let [reply (.sendReadOnly (.async client)
+                                    (.getMessage CounterCommand/GET))]
+            (try
+              (let [response (.get reply *operation-timeout-ms* TimeUnit/MILLISECONDS)]
+                (if (.isSuccess response)
+                  (let [value (extract-counter-value response)]
+                    (assoc op :type :ok :value (or value 0)))
+                  (assoc op :type :fail :error (.toString response))))
+              (catch java.util.concurrent.TimeoutException _
+                (assoc op :type :info :error "timeout"))
+              (catch Exception e
+                (assoc op :type :fail :error (.getMessage e)))))
+          
+          (assoc op :type :fail :error (str "Unknown operation: " (:f op))))
+        
+        (catch Exception e
+          (let [msg (.getMessage e)]
+            (warn "Operation failed:" msg)
+            (assoc op :type :fail :error msg))))))
+  
+  (teardown! [this test]
+    (info "Tearing down Ratis client")
+    this)
+  
+  (close! [this test]
+    (info "Closing Ratis client")
+    (when client
+      (try
+        (.close client)
+        (catch Exception e
+          (warn "Error closing client:" (.getMessage e)))))))
+
+;; Keep MockRatisClient for backward compatibility during testing
 (defrecord MockRatisClient [counter]
   client/Client
   
@@ -53,6 +178,11 @@
     (info "Closing mock Ratis client")))
 
 (defn counter-client
+  "Create a new Ratis counter client for testing"
+  []
+  (RatisCounterClient. nil nil))
+
+(defn mock-counter-client
   "Create a new mock counter client for testing"
   []
   (MockRatisClient. nil))
@@ -71,4 +201,4 @@
 (defn get-op
   "Generate a get operation"
   [_ _]
-  {:type :invoke :f :get :value nil}) 
+  {:type :invoke :f :get :value nil})
